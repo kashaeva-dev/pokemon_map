a98961b6306f6bbb42b2d81fc9465235fc78c192
Nataliya Kashaeva <n.a.kashaeva@gmail.com>
2023-05-10 10:59:03 +0300

    Show Pokemon name in UI

--- pokemon_entities/models.py
+++ pokemon_entities/models.py
@@ -3,3 +3,6 @@ from django.db import models  # noqa F401
 # your models here
 class Pokemon(models.Model):
     title = models.CharField(max_length=200, verbose_name="Название")
+
+    def __str__(self):
+        return str(self.title)
